2a3581c64b4242f9c7e9408ec325bc3259431b6d
Troy Curnel <NearSingularity@troycurnel.com>
2018-02-01 13:37:20 -0800

    Add stripe:retrieve-plan
    Add stripe:list-plans
    
    Add basic tests

---
+++ constants/plan.schema.js
@@ -0,0 +1,15 @@
+// Stripe API subscription object -- see https://stripe.com/docs/api#retrieve_plan for full documentation.
+module.exports = {
+  'id': 'basic',
+  'object': 'plan',
+  'amount': 3999,
+  'created': 1517470323,
+  'currency': 'usd',
+  'interval': 'month',
+  'interval_count': 1,
+  'livemode': false,
+  'metadata': {},
+  'name': 'basic',
+  'statement_descriptor': '',
+  'trial_period_days': 0
+};
